0e2119ee5c96824307707ac37f227761da5d47df
LeonardoCosano <leonardocosanocontact@gmail.com>
2025-12-03 23:37:24 +0100

    first challenge completed

--- day1/1.py
+++ day1/1.py
@@ -1,16 +1,18 @@
 def identifyRotation(line):
 	amount = int(line[1:])
 	rotation = line[0]
-	if rotation == "L":
-		rotation = 0
-	else:
-		rotation = 1
 
 	return rotation, amount
 
 def updateArrow(currentArrowPosition, rotation, amount):
+
+	while amount > 99:
+		currentArrowPosition = updateArrow(currentArrowPosition, rotation, 99)
+		amount = amount - 99
+		pass
+
 	futureArrowPosition = 0
-	if rotation == 0:
+	if rotation == "L":
 		futureArrowPosition = currentArrowPosition - amount
 	else:
 		futureArrowPosition = currentArrowPosition + amount
@@ -50,8 +52,6 @@ def main():
 	  rotation, amount = identifyRotation(line)
 	  arrowPosition = updateArrow(arrowPosition, rotation, amount)
 	  password = updatePassword(arrowPosition, password)
-	  if amount == 79 or amount == 148:
-	  	print ("Rotating " + str(amount) + " positions to the " + line[0] + " so new position is " + str(arrowPosition))
 	  
 	print ("Number of times the safe's dial pointed 0 is " + str(password))
 
